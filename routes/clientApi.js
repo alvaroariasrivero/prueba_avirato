85e9dfb867008da4e32483b7fe805712878e39f0
Álvaro Arias Rivero <alvaroariasrivero92@gmail.com>
2022-05-14 13:51:55 +0200

    comment routes that doesn't work

--- routes/clientApi.js
+++ routes/clientApi.js
@@ -4,9 +4,9 @@ const clientApi = require('../controllers/clientApi');
 routes.post('/client/post', clientApi.signUpClient)
     .get('/client/get', clientApi.getAllClients)
     .get('/client/get/:id?', clientApi.getClientById)
-    .get('/client/get/search/:filter', clientApi.getOneClient)
-    .put('/client/put/:email', clientApi.updateClientByEmail)
-    .delete('/client/delete', clientApi.deleteOneClient)
+    // .get('/client/get/search/:filter', clientApi.getOneClient)
+    // .put('/client/put/:email', clientApi.updateClientByEmail)
+    // .delete('/client/delete', clientApi.deleteOneClient)
     .post('/auth/login', clientApi.loginClient)
 
 module.exports = routes;
